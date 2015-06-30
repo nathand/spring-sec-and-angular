a116b03bc2efc50706ce535cbbdf4a9651968f85
n <dorman.nathan@gmail.com>
2015-06-29 22:30:12 -1000

    Updated to store sessions in redis. Not working cross domain yet.

--- resource/src/main/java/demo/ResourceApplication.groovy
+++ resource/src/main/java/demo/ResourceApplication.groovy
@@ -1,12 +1,16 @@
-package demo
+package demo;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableRedisHttpSession
 class ResourceApplication {
 
   @RequestMapping('/')
@@ -16,5 +20,10 @@ class ResourceApplication {
 
   static void main(String[] args) {
     SpringApplication.run ResourceApplication, args
+  }
+
+  @Bean
+  HeaderHttpSessionStrategy sessionStrategy() {
+    new HeaderHttpSessionStrategy();
   }
 }
--- ui/src/main/java/demo/UiApplication.java
+++ ui/src/main/java/demo/UiApplication.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -12,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,13 +26,16 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
 @RestController
+@EnableRedisHttpSession
 public class UiApplication {
 
   @RequestMapping("/resource")
@@ -43,6 +48,11 @@ public class UiApplication {
   @RequestMapping("/user")
   public Principal user(Principal user) {
     return user;
+  }
+  @RequestMapping("/token")
+  @ResponseBody
+  public Map<String,String> token(HttpSession session) {
+    return Collections.singletonMap("token", session.getId());
   }
 
     public static void main(String[] args) {
